ebdea7050378c89b76726a640c4d1edf2f9c815f
Sergej Keselj <keshelj@hotmail.com>
2017-10-14 17:24:09 +0200

    add default app page template

--- Django.Core/app/urls.py
+++ Django.Core/app/urls.py
@@ -4,7 +4,12 @@ from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # url(r'^$', views.project_list, name='project'),
+
+# BEGIN_OF_CODE_GENERATION
+
+# END_OF_CODE_GENERATION
+
+    url(r'^$', views.home_page, name='home_page'),
     #url(r'^user/profile$', views.user_profile, name='user-profile'),
     #url(r'^user/update/$', views.user_update, name='user_update'),
     #url(r'^user/password/$', views.change_password, name='user_password_change'),
--- Django.Core/app/views.py
+++ Django.Core/app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+# BEGIN_OF_CODE_GENERATION
+
+# END_OF_CODE_GENERATION
+
+
+@login_required()
+def home_page(request):
+    template_name = 'app/home_page.html'
+    return render(request, template_name, {'user': request.user})
