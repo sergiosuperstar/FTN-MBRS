1facbade080b6cb9525d075a658d52fb38794b41
markoIvetic93 <markoIvetic93@gmail.com>
2017-10-14 12:08:13 +0200

    Zapoceto parsiranje.

--- Generator/execute.py
+++ Generator/execute.py
@@ -1,15 +1,73 @@
 from textx.metamodel import metamodel_from_file
 from textx.export import metamodel_export, model_export
-import pydot
+from jinja2 import Environment, PackageLoader, select_autoescape
+import pydot, os
 
-if __name__ == '__main__':
+class Items(object):
+    def __init__(self):
+        self.model = {}
 
-    test_mm = metamodel_from_file('grammar.txt')
-    metamodel_export(test_mm, 'test_meta.dot')
-    graph = pydot.graph_from_dot_file('test_meta.dot')
-    graph[0].write_png('test_meta.png')
+    def interpreter_for_model(self, model):
+        model_items = []
+        enum_items = []
 
-    test_model = test_mm.model_from_file('test.model')
-    model_export(test_model, 'test_model.dot')
-    graph = pydot.graph_from_dot_file('test_model.dot')
-    graph[0].write_png('test_model.png')
+        for item in model.items:
+            if item.modelItem is not None:
+                model_items.append(item.modelItem)
+            elif item.enumItem is not None:
+                enum_items.append(item.enumItem)
+
+        models_model = []
+        for model_item in model_items:
+            one_model = {}
+            one_model["modelName"] = model_item.name
+            properties = {}
+            baseProperties = []
+            customProperties = []
+            for property in model_item.properties:
+                baseProperty = {}
+                customProperty = {}
+                ''' one function? '''
+                if property.type.baseType is not None:
+                    baseProperty["propertyName"] = property.name
+                    baseProperty["propertyType"] = property.type.baseType
+                    '''options = []
+                    option = {}
+                    for opt in property.options:'''
+                    baseProperties.append(baseProperty)
+                elif property.type.customType is not None:
+                    customProperty["propertyName"] = property.name
+                    customProperty["propertyType"] = property.type.customType
+                    customProperties.append(customProperty)
+            properties["baseProperties"] = baseProperties
+            properties["customProperties"] = customProperties
+            one_model["modelProperties"] = properties
+            models_model.append(one_model)
+        self.model["modelItems"] = models_model
+
+        models_enum = []
+        for enum_item in enum_items:
+            one_enum = {}
+            one_enum["enumName"] = enum_item.name
+            '''enumItems = enum_item.enumItems
+            one_enum["enumItems"] = enumItems'''
+            models_enum.append(one_enum)
+        self.model["enumItems"] = models_enum
+
+        return self.model
+
+def execute(path, grammar_file_name, example_file_name):
+
+    meta_path = os.path.join(path, grammar_file_name)
+    meta_name = os.path.splitext(meta_path)[0]
+    metamodel = metamodel_from_file(meta_path)
+
+    model_path = os.path.join(path, example_file_name)
+    model_name = os.path.splitext(model_path)[0]
+    model = metamodel.model_from_file(model_path)
+
+    items = Items()
+    return items.interpreter_for_model(model)
+
+query_set = execute(os.path.split(__file__)[0], 'grammar.txt', 'test.model')
+print(query_set)
